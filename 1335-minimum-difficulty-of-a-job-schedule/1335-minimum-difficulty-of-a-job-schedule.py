42eae2f16161a3ff91403703a77b453692196876
rho5900 <89143547+rho5900@users.noreply.github.com>
2026-02-13 00:23:29 -0800

    Time: 339 ms (71.48%), Space: 24.1 MB (11.04%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        day = 0
+        #len(jobDifficulty) - d-1
+        #left 
+        #day were on
+        if len(jobDifficulty) < d:
+            return -1
+        @lru_cache(2000)
+        
+        def dfs(left,days):
+            
+            if days == d:
+                return 0 if left == len(jobDifficulty) else float('inf')
+            if left >= len(jobDifficulty):
+                return float('inf')
+            #len(jobDifficulty) - d - days - 1
+            maxs = float('-inf')
+            ars = []
+            for x in range(left,len(jobDifficulty) - (d - days) + 1):
+                if jobDifficulty[x] > maxs:
+                    maxs = jobDifficulty[x]
+                ars.append(maxs+dfs(x+1,days + 1))
+
+            return min(ars)
+        
+        return dfs(0,0)
+                
